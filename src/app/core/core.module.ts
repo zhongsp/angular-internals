d71b3345569854ac621c031976ab8f544e6ae68a
zhongsp <patrick.zhongsp@gmail.com>
2018-08-18 07:38:04 +0800

    init core module and breakcrumbs component

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    BreadcrumbsComponent
+  ]
+})
+export class CoreModule { }
